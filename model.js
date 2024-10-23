bcf1e9077cdf294b16b1dcd0fd64f4b5a7d02a81
nsicart <nsicart.moragas@gmail.com>
2024-10-23 20:09:02 +0200

    Create model.js

---
+++ model.js
@@ -0,0 +1,7 @@
+document.getElementById('addPointButton').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'flex';
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
