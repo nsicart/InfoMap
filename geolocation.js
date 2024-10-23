36ff034b1f3789efefdc2838ba260c49517333a7
nsicart <nsicart.moragas@gmail.com>
2024-10-23 20:08:40 +0200

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -1,16 +1,21 @@
-if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(updateLocation, handleError, { enableHighAccuracy: true });
-} else {
-    alert("El teu navegador no suporta la geolocalització.");
-}
+var pointsOfInterest = [];
 
 function updateLocation(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
     marker.setLatLng([lat, lng]);
     map.setView([lat, lng], 13);
-}
 
-function handleError(error) {
-    console.error("Error obtenint la ubicació:", error);
+    pointsOfInterest.forEach(function(point) {
+        var distance = map.distance([lat, lng], [point.lat, point.lng]);
+        console.log("Distància al punt d'interès (", point.lat, point.lng, "): ", distance);
+        if (distance < 200 && !point.audioPlayed) {
+            playAudio(point.audioUrl);
+            point.audioPlayed = true;
+        }
+    });
+
+    if (!pointsOfInterest.some(point => point.source === 'OSM')) {
+        fetchPointsOfInterest(lat, lng);
+    }
 }
