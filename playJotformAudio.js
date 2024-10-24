12554bfd0955900d4f00eef2425a2938b73daede
nsicart <nsicart.moragas@gmail.com>
2024-10-24 11:50:21 +0200

    Update playJotformAudio.js

--- playJotformAudio.js
+++ playJotformAudio.js
@@ -1,6 +1,10 @@
-function playJotformAudio(point) {
-    if (audioEnabled && point.source === 'JotForm' && point.audioUrl) {
-        var audio = new Audio(point.audioUrl);
-        audio.play();
+function playJotformAudio(point, currentLat, currentLng) {
+    if (audioEnabled && point.source === 'JotForm' && point.audioUrl && !point.audioPlayed) {
+        var distance = map.distance([currentLat, currentLng], [point.lat, point.lng]);
+        if (distance < 50) { // Si estem a menys de 50 metres del punt
+            var audio = new Audio(point.audioUrl);
+            audio.play();
+            point.audioPlayed = true; // Marcar l'àudio com a reproduït
+        }
     }
 }
