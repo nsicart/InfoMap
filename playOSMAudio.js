32a76130b7dc893593074f1c48508fe01bdd03e4
nsicart <nsicart.moragas@gmail.com>
2024-10-24 12:05:30 +0200

    Update playOSMAudio.js

--- playOSMAudio.js
+++ playOSMAudio.js
@@ -1,7 +1,12 @@
-function playOSMAudio(point) {
-    if (audioEnabled && point.source === 'OSM' && point.description) {
-        const speech = new SpeechSynthesisUtterance(point.description);
-        speech.lang = selectedLanguage;
-        window.speechSynthesis.speak(speech);
+function playOSMAudio(point, currentLat, currentLng) {
+    if (audioEnabled && point.source === 'OSM' && point.description && !point.audioPlayed) {
+        var distance = map.distance([currentLat, currentLng], [point.lat, point.lng]);
+        if (distance < 50) { // Si estem a menys de 50 metres
+            const speech = new SpeechSynthesisUtterance(point.description);
+            speech.lang = selectedLanguage;
+            window.speechSynthesis.speak(speech);
+            point.audioPlayed = true; // Marcar l'àudio com a reproduït
+        }
     }
 }
+
