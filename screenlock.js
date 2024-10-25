5aa5d21c77640b993908478fc4779f60500eceb5
nsicart <nsicart.moragas@gmail.com>
2024-10-25 20:29:43 +0200

    Update screenlock.js

--- screenlock.js
+++ screenlock.js
@@ -1,13 +1,29 @@
 let wakeLock = null;
 
 async function requestWakeLock() {
-    try {
-        wakeLock = await navigator.wakeLock.request('screen');
-        console.log('Wake Lock activat!');
-    } catch (err) {
-        console.error(`${err.name}, ${err.message}`);
+    if ('wakeLock' in navigator) { // Verifica que l'API de Wake Lock està disponible
+        try {
+            wakeLock = await navigator.wakeLock.request('screen');
+            console.log('Wake Lock activat!');
+
+            // Escolta l'esdeveniment de quan es perd el Wake Lock
+            wakeLock.addEventListener('release', () => {
+                console.log('Wake Lock s\'ha alliberat.');
+            });
+        } catch (err) {
+            console.error(`Error activant el Wake Lock: ${err.name}, ${err.message}`);
+        }
+    } else {
+        console.log('API de Wake Lock no disponible en aquest navegador.');
     }
 }
+
+// Reactiva el Wake Lock si la pàgina torna a ser visible
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+        requestWakeLock();
+    }
+});
 
 // Activar el Wake Lock en carregar la pàgina
 document.addEventListener('DOMContentLoaded', () => {
